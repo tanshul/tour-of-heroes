cc07afd3c6970fde212b9674df43e2ac7bbc100f
Tanshul Kumar <subzero369@gmail.com>
2016-09-17 16:54:24 +1000

    Services and Promises

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,6 @@
-import { Component } from '@angular/core';
-
-export class Hero{
-    id: number;
-    name: string;
-}
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-app',
@@ -71,32 +54,34 @@ const HEROES: Hero[] = [
   }
 `]
 ,
-    template: `
-    <div *ngIf="selectedHero">
-    <h2>{{selectedHero.name}} details!</h2>
-<div><label>id: </label>{{selectedHero.id}}</div>
-<div>
-    <label>name: </label>
-    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-</div>
-</div>
+    template: `<my-hero-detail [hero]="selectedHero"></my-hero-detail>
 
-<h2>My Heroes</h2>
-<ul class="heroes">
-  <li [class.selected]="hero === selectedHero"  *ngFor="let hero of heroes" (click)="onSelect(hero)">
-    <span class="badge">{{hero.id}}</span> {{hero.name}}
-  </li>
-</ul>
-
-
-    `
+        <h2>My Heroes</h2>
+        <ul class="heroes">
+        <li [class.selected]="hero === selectedHero"  *ngFor="let hero of heroes" (click)="onSelect(hero)">
+            <span class="badge">{{hero.id}}</span> {{hero.name}}
+        </li>
+        </ul>
+    `,
+    providers:[HeroService]
 })
-export class AppComponent { 
+export class AppComponent implements OnInit{ 
+
+    constructor(private heroService: HeroService){}
+
+ngOnInit():void{
+this.getHeroes();
+}
+
     title:string = "Tour of Heroes";
-    heroes = HEROES;
+    heroes : Hero[];
     selectedHero: Hero;
 
     onSelect(hero:Hero):void{
         this.selectedHero = hero;
     }
+      getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
 }
